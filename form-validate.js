5e5bef12b9c54c5663b2048baa9d4b70ad70cfe6
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-06 20:08:35 +0500

    Error message shown and form operable on both mobile and desktop versions

--- form-validate.js
+++ form-validate.js
@@ -1,4 +1,5 @@
 const submitBtn = document.querySelector('.footerButton');
+const submitBtn2 = document.querySelector('.footerButtonMobile');
 const validateEmail = (email) => {
   if (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)) {
     return true;
@@ -10,7 +11,16 @@ submitBtn.addEventListener('click', (event) => {
   const emailVal = document.getElementById('mail').value;
   
   if (!validateEmail(emailVal)) {
-    alert("Invalid format");
+    let label=document.querySelector('.error').innerHTML="Email should be written in small case";
+    event.preventDefault();
+  }
+});
+
+submitBtn2.addEventListener('click', (event) => {
+  const emailVal = document.getElementById('mail').value;
+  
+  if (!validateEmail(emailVal)) {
+    let label=document.querySelector('.error').innerHTML="Email should be written in small case";
     event.preventDefault();
   }
 });
